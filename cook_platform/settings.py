b7ff363eaea5e38a1fb1965289e32153d017b458
Gokul6204 <gogul062004@gmail.com>
2025-11-07 13:31:16 +0530

    Configured Cloudinary for media storage

--- cook_platform/settings.py
+++ cook_platform/settings.py
@@ -118,7 +118,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
